b6e742b4d5025ce8a942a3505143b61fbeeac51b
sajicode <sajioloye@gmail.com>
2020-05-10 09:07:06 +0100

    add health check route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import UserRouter from './Users';
 import TeamRouter from './Teams';
 import FixtureRouter from './Fixture';
+import Utils from 'src/utils/utils';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
+router.use('/health', (req: Request, res: Response) => {
+  const message = 'League Server is up & Running';
+  return Utils.successResponse(res, [], message);
+});
 router.use('/users', UserRouter);
 router.use('/teams', TeamRouter);
 router.use('/fixtures', FixtureRouter);
